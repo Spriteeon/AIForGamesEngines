d52d2327c2f3b5880101fb77cacb2d22c58ea738
FranPorritt <francescaporritt1@gmail.com>
2021-03-04 16:20:54 +0000

    Player still won't move with platforms :(

--- AIForGamesEnginesProject/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ AIForGamesEnginesProject/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -255,5 +255,10 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
+
+        public void OnPlatform(Vector3 offset)
+        {
+            transform.position += offset;
+        }
     }
 }
