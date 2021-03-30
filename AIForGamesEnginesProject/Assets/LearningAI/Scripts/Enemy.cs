7f407b7893451eece2862f447c6bbd6c458839cd
Malachi Coward <60357765+MalCoward@users.noreply.github.com>
2021-03-30 18:48:37 +0100

    Started Learning and Enemy Scripts

---
+++ AIForGamesEnginesProject/Assets/LearningAI/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int startingHealth = 100;
+
+    private int currentHealth;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        currentHealth = startingHealth;
+    }
+
+    public void GetShot(int damage)
+	{
+        ApplyDamage(damage);
+	}
+
+    private void ApplyDamage(int damage)
+	{
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+		{
+            Die();
+		}
+	}
+
+    private void Die()
+	{
+        Debug.Log(message: "I Died!");
+        Respawn();
+	}
+
+    private void Respawn()
+	{
+        currentHealth = startingHealth;
+        transform.position = startPosition;
+	}
+
+	#region Debug
+	private void OnMouseDown()
+	{
+        GetShot(startingHealth);
+	}
+	#endregion
+}
---
+++ AIForGamesEnginesProject/Assets/LearningAI/Scripts/ShootingAgent.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class ShootingAgent : Agent
+{
+	public Transform shootingPoint;
+	public int minStepsBetweenShots = 50;
+	public int damage = 100;
+
+	private bool shotAvailable = true;
+	private int stepsUntilShotIsAvailable = 0;
+
+	private void Shoot(Vector3 direction)
+	{
+		if (!shotAvailable)
+			return;
+
+		int layerMask = 1 << LayerMask.NameToLayer("Enemy");
+
+		Debug.Log(message: "Shot!");
+		Debug.DrawRay(start: shootingPoint.position, dir: direction * 200.0f, Color.green, duration: 2.0f);
+
+		if (Physics.Raycast(origin: shootingPoint.position, direction, out var hit, maxDistance: 200.0f, layerMask))
+		{
+			hit.transform.GetComponent<Enemy>().GetShot(damage);
+		}
+
+		shotAvailable = false;
+		stepsUntilShotIsAvailable = minStepsBetweenShots;
+	}
+
+	private void FixedUpdate()
+	{
+		if (!shotAvailable)
+		{
+			stepsUntilShotIsAvailable--;
+			if (stepsUntilShotIsAvailable <= 0)
+			{
+				shotAvailable = true;
+			}
+		}
+	}
+
+    public override void OnActionReceived(float[] vectorAction)
+	{
+		base.OnActionReceived(vectorAction);
+	}
+
+	public override void CollectObservations(VectorSensor sensor)
+	{
+		base.CollectObservations(sensor);
+	}
+
+	public override void Initialize()
+	{
+		base.Initialize();
+	}
+
+	public override void Heuristic(float[] actionsOut)
+	{
+		//transform.rotation.SetLookRotation();
+	}
+
+	public override void OnEpisodeBegin()
+	{
+		base.OnEpisodeBegin();
+	}
+
+	#region Debug
+	private void OnMouseDown()
+	{
+		Shoot(-transform.right);
+	}
+	#endregion
+}
