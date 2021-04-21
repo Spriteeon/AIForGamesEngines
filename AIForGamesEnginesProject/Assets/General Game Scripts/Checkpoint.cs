157c300c602a2301fd4ada25b270373801b1f05d
FranPorritt <francescaporritt1@gmail.com>
2021-04-21 17:00:52 +0100

    Started on main level
    - Added checkpoints and respawning
    - Made some platform prefabs so we can just drag and drop the level

---
+++ AIForGamesEnginesProject/Assets/General Game Scripts/Checkpoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    Collider checkpointCollider;
+
+    [SerializeField]
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkpointCollider = GetComponent<Collider>();
+
+        if (!player)
+        {
+            throw new System.Exception("Checkpoint missing player reference!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player hit checkpoint!");
+            player.SetCurrentCheckpoint(this.gameObject);
+        }
+    }
+}
---
+++ AIForGamesEnginesProject/Assets/General Game Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!player) // player null
+        {
+            throw new System.Exception("Player Null!");
+        }
+    }   
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player respawning");
+        GameObject checkpoint = player.GetComponent<PlayerController>().GetCurrentCheckpoint();
+
+        if (checkpoint)
+        {
+            Debug.Log(checkpoint.transform.position);
+            player.GetComponent<FirstPersonController>().m_CharacterController.enabled = false;
+            player.GetComponent<FirstPersonController>().m_CharacterController.transform.position = checkpoint.transform.position + new Vector3(-10,5,0); // Spawns player above spawn point so they fall to platform
+            player.GetComponent<FirstPersonController>().m_CharacterController.enabled = true;
+        }
+        else
+        {
+            // Spawn at start??
+        }
+    }
+}
