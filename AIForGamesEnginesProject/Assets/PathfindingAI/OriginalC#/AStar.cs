76d37b9e5ce9d4ace275b687afeb6f9bb8fda162
Kittyjelly19 <49998740+Kittyjelly19@users.noreply.github.com>
2021-04-21 17:34:30 +0100

    Finally working!!! Need to sort variable names etc and add player input/enemy movement
    sorted stuff into folders, need to tidy up and delete unused stuff.

---
+++ AIForGamesEnginesProject/Assets/PathfindingAI/OriginalC#/AStar.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStar : MonoBehaviour
+{
+    public Transform seeker, target;
+
+    LevelGrid grid;
+
+    //public List<Node> finalPath;
+     
+
+    void Awake()
+    {
+        grid = GetComponent<LevelGrid> ();
+    }
+
+    void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        //The start and end of the path. Used to determin the path that needs calculating. 
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        //groups of open and closed nodes used in pathfinding calculations. 
+        //Open = is yet to be checked but is in range. Closed = has been checked and processed. 
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        openSet.Add(startNode);
+
+        while(openSet.Count > 0) 
+        {
+            Node node = openSet[0];
+            for(int i = 1; i < openSet.Count; i++) 
+            {
+                
+                if(openSet[i].fCost < node.fCost || openSet[i].fCost == node.fCost) 
+                {
+                    if (openSet[i].hCost < node.hCost)
+                    {
+                        node = openSet[i];
+                    }
+                        
+                    
+                }
+            }
+               
+            openSet.Remove(node);
+            closedSet.Add(node);
+            
+            if(node == targetNode) 
+            {
+                RetracePath(startNode,targetNode);
+                 return;
+            }
+
+
+            foreach(Node neighbour in grid.GetNeighbours(node)) 
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                {
+                    continue;
+                   
+                }
+
+                int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
+               if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+               {
+                  neighbour.gCost = newCostToNeighbour;
+                  neighbour.hCost = GetDistance(neighbour, targetNode);
+                  neighbour.parent = node;
+
+                 if (!openSet.Contains(neighbour))
+                 {
+                        openSet.Add(neighbour);
+                 }    
+               }
+            }
+        }  
+    }
+
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while(currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+        grid.path = path;
+
+        //grid.FPath = finalPath;
+    }
+
+
+
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if(dstX > dstY)
+        {
+            return 14*dstY + 10* (dstX-dstY);
+        }
+           
+        return 14*dstX + 10 * (dstY-dstX);
+        
+    }
+}
