caae07dc2b635fba8a58dfd687729db57e7ed046
Malachi Coward <60357765+MalCoward@users.noreply.github.com>
2021-04-21 20:20:35 +0100

    Obstacle Agent learnt to go in correct direction

--- AIForGamesEnginesProject/Assets/LearningAI/Scripts/Obstacle/ObstacleAgent.cs
+++ AIForGamesEnginesProject/Assets/LearningAI/Scripts/Obstacle/ObstacleAgent.cs
@@ -15,32 +15,15 @@ public class ObstacleAgent : Agent
 	private EnvironmentParameters environmentParameters;
 
 	public event Action onEnvironmentReset;
+	public GameObject finish;
 
-	private void FixedUpdate()
-	{
-		
-	}
-
-	public override void OnActionReceived(float[] vectorAction)
-	{
-		Rb.velocity = new Vector3(vectorAction[0] * speed, 0f, vectorAction[1] * speed);
-	}
-
-	public override void CollectObservations(VectorSensor sensor)
-	{
-		base.CollectObservations(sensor);
-	}
+	float distanceToFinish;
+	float oldDistanceToFinish;
 
 	public override void Initialize()
 	{
 		startingPosition = transform.position;
 		Rb = GetComponent<Rigidbody>();
-	}
-
-	public override void Heuristic(float[] actionsOut)
-	{
-		actionsOut[0] = Input.GetAxis("Horizontal");
-		actionsOut[1] = Input.GetAxis("Vertical");
 	}
 
 	public override void OnEpisodeBegin()
@@ -50,6 +33,64 @@ public class ObstacleAgent : Agent
 		Debug.Log(message: "Episode Begin");
 		transform.position = startingPosition;
 		Rb.velocity = Vector3.zero;
+
+		distanceToFinish = Vector3.Distance(gameObject.transform.position, finish.transform.position);
+		oldDistanceToFinish = distanceToFinish;
+	}
+
+	private void FixedUpdate()
+	{
+		distanceToFinish = Vector3.Distance(gameObject.transform.position, finish.transform.position);
+		if(distanceToFinish < oldDistanceToFinish)
+        {
+			AddReward(1f);
+		}
+        else
+        {
+			AddReward(-1f);
+		}
+		oldDistanceToFinish = distanceToFinish;
+	}
+
+	public override void OnActionReceived(float[] vectorAction)
+	{
+		Rb.velocity = new Vector3(vectorAction[0] * speed, 0f, vectorAction[1] * speed);
+	}
+
+	public override void CollectObservations(VectorSensor sensor)
+	{
+		sensor.AddObservation(Vector3.Distance(finish.transform.position, gameObject.transform.position));
+		//base.CollectObservations(sensor);
+	}
+
+	public override void Heuristic(float[] actionsOut)
+	{
+		actionsOut[0] = Input.GetAxis("Horizontal");
+		actionsOut[1] = Input.GetAxis("Vertical");
+	}
+
+	void OnCollisionEnter(Collision collision)
+    {
+		if (collision.gameObject.CompareTag("enemy"))
+        {
+			AddReward(-1f);
+			//EndEpisode();
+        }
+		if (collision.gameObject.CompareTag("wall"))
+		{
+			AddReward(-1f);
+			//EndEpisode();
+		}
+		//if (collision.gameObject.CompareTag("die"))
+		//{
+		//	AddReward(-0.5f);
+		//	EndEpisode();
+		//}
+		if (collision.gameObject.CompareTag("finish"))
+        {
+			AddReward(100f);
+			EndEpisode();
+        }
 	}
 
 	private void Reset()
