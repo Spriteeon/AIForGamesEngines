3a189b5a74b939ba2d3fbff1c00de0cb2f7f0f75
FranPorritt <francescaporritt1@gmail.com>
2021-05-11 11:47:47 +0100

    Game music

---
+++ AIForGamesEnginesProject/Assets/General Game Scripts/GameMusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusic : MonoBehaviour
+{
+    private static GameMusic instance = null;
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        if (instance == this) return;
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
